b7e73fb40298ecdb4ff6594d03020e977fc132de
anilsaidonga <anilsaidonga@outlook.com>
2025-09-14 19:25:00 +0530

    code

--- dsa/algorithms/Try.java
+++ dsa/algorithms/Try.java
@@ -3,21 +3,6 @@ package algorithms;
 public class Try {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		int[] arr = {1, 2, 3, 4, -5};
-		System.out.println(arr);
-=======
-		int[] arr = {1, 2, 3};
-		int n = arr.length;
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = i; j < n; j++)
-			{
-				System.out.print(arr[j] + " ");
-			}
-			System.out.println();
-		}
->>>>>>> 96f69942f36ff05275a24eeb6704b5e8b9a62363
 	}
 
 }
--- dsa/recursion/SubsetSum.java
+++ dsa/recursion/SubsetSum.java
@@ -1,45 +1,5 @@
 package recursion;
 
-<<<<<<< HEAD
-import java.util.*;
-
-public class SubsetSum {
-	
-	/*
-	 * brute force approach
-	 * time complexity: O( 2 ^ n * n ) ( there are 2 ^ n sub sets * and you copy every sub set of size n )
-	 * space complexity: O(n) + O( 2 ^ n * n ) ( there is recursion depth of n + and there are 2 ^ n sub sets )
-	 */
-	
-	public static boolean helper(int[] arr, int ind, List<Integer> curr, int sum)
-	{
-		if (ind >= arr.length)
-		{
-			int subSetSum = 0;
-			for (Integer i : curr) subSetSum += i;
-			return subSetSum == sum;
-		}
-		
-		// include wala case
-		curr.add(arr[ind]);
-		if (helper(arr, ind + 1, curr, sum)) return true;
-		curr.remove(curr.size() - 1);
-		
-		// exclude wala case        
-		if (helper(arr, ind + 1, curr, sum)) return true;
-		
-		return false;
-	}
-
-	public static void main(String[] args) {
-		//int[] arr = {3, 34, 4, 12, 5, 2};	
-		int[] arr = {1, 2, 3};
-		List<Integer> curr = new ArrayList<>();
-		List<List<Integer>> res = new ArrayList<>();
-		System.out.println(helper(arr, 0, curr, 6));
-	}
-
-=======
 public class SubsetSum {
 	
 //	public static boolean helper(int ind, int[] arr, int sum)
@@ -79,5 +39,4 @@ public class SubsetSum {
 		Boolean[][] dp = new Boolean[arr.length][5 + 1];
 		System.out.println(helper(arr.length - 1, arr, 5, dp));
 	}
->>>>>>> 96f69942f36ff05275a24eeb6704b5e8b9a62363
 }
